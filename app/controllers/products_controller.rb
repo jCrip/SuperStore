f59e01d58e685348665e091a479cff9d161cd0b7
Juan Cristobal Pazos <juancri@lalegion.cl>
2015-10-24 15:48:25 -0300

    add category to new product
    Signed-off-by: Juan Cristobal Pazos <juancri@lalegion.cl>

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :price, :price, :stock)
+      params.require(:product).permit(:name, :description, :price, :price, :stock, :category_id)
     end
 end
