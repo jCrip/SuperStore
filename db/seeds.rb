d2bd707de6fd53e214091ff232790953144572cf
Juan Cristobal Pazos <juancri@lalegion.cl>
2015-10-27 19:52:07 -0300

    better seed
    Signed-off-by: Juan Cristobal Pazos <juancri@lalegion.cl>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,13 @@
+Cart.destroy_all
+ProductOrder.destroy_all
+Order.destroy_all
 Product.destroy_all
 User.destroy_all
 Category.destroy_all
 
-users = []
 
-users << User.create(
+
+admin = User.create(
   name: 'Juan Cristobal',
   lastname: 'Pazos',
   username: 'juancri',
@@ -12,14 +15,19 @@ users << User.create(
   password: 'password',
   role: 0
 )
-users << User.create(
-  name: 'Maida',
-  lastname: 'Claro',
-  username: 'mai_clear',
-  email: 'client@ecommerce.com',
-  password: 'password',
-  role: 1
-)
+
+users = []
+
+(1..10).each do |i|
+  users << User.create(
+    name: Faker::Name.first_name,
+    lastname: Faker::Name.last_name,
+    username: Faker::Internet.user_name,
+    email: "client_#{i}@mail.com",
+    password: 'password',
+    role: 1
+  )
+end
 
 
 categories = []
@@ -39,12 +47,44 @@ categories << Category.create(name: 'Others')
 
 products = []
 
-80.times do |p|
+40.times do |p|
   products << Product.create(
     name: Faker::Commerce.product_name,
     description: Faker::Lorem.paragraph(10, true, 10),
     price: Faker::Commerce.price,
-    stock: Faker::Number.between(1, 20)
+    stock: Faker::Number.between(1, 20),
+    category: categories[rand(categories.length)],
+    remote_image_url: 'http://lorempixel.com/1200/800'
   )
+end
 
+5.times do |i|
+  cart = Cart.new
+  cart.user = users[0]
+  cart.product = products.sample
+  cart.quantity = Faker::Number.between(1, 10)
+  cart.save
+end
+
+4.times do |i|
+  cart = Cart.new
+  cart.user = users[1]
+  cart.product = products.sample
+  cart.quantity = Faker::Number.between(1, 10)
+  cart.save
+end
+
+
+order = Order.new
+order.user = users[1]
+order.status = 0
+order.save
+
+
+6.times do |i|
+  po = ProductOrder.new
+  po.order = order
+  po.product = products.sample
+  po.quantity = Faker::Number.between(1, 10)
+  po.save
 end
