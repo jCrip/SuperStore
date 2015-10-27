a8857155eef69db7d6eb99b1b1413c70694573ee
Juan Cristobal Pazos <juancri@lalegion.cl>
2015-10-27 19:50:45 -0300

    add devise strong params and cancan rescue
    Signed-off-by: Juan Cristobal Pazos <juancri@lalegion.cl>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: 'Tu no tienes permiso para hacer esto!!! FUERAA'
+  end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << [:name, :lastname, :username]
+      devise_parameter_sanitizer.for(:account_update) << [:name, :lastname, :username]
+    end
+
+    def require_admin
+      authenticate_user!
+      redirect_to root_path unless current_user.admin?
+    end
 end
