941e5e8fda0e77ab6e32adae54e52c76e8b57e67
Juan Cristobal Pazos <juancri@lalegion.cl>
2015-11-04 10:48:33 -0300

    Admin confirmation mail preview created
    Signed-off-by: Juan Cristobal Pazos <juancri@lalegion.cl>

--- test/mailers/previews/order_mailer_preview.rb
+++ test/mailers/previews/order_mailer_preview.rb
@@ -3,4 +3,8 @@ class OrderMailerPreview < ActionMailer::Preview
   def confirmation_email_preview
     OrderMailer.confirmation_email(Order.last, User.last)
   end
+
+  def confirmation_admin_preview
+    OrderMailer.confirmation_admin(Order.last)
+  end
 end
