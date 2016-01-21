63bda75e9cc60b1f9df025a454186042d1443184
Juan Cristobal Pazos <juancri@lalegion.cl>
2016-01-21 13:23:16 -0300

    code cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,15 @@ Rails.application.routes.draw do
   devise_for :users, skip: [:sessions, :registrations], controllers: { omniauth_callbacks: :omniauth_callbacks, registrations: :registrations }
 
   devise_scope :user do
-    get    "login"   => "devise/sessions#new",         as: :new_user_session
-    post   "login"   => "devise/sessions#create",      as: :user_session
-    delete "signout" => "devise/sessions#destroy",     as: :destroy_user_session
+    get    "login"       => "devise/sessions#new",     as: :new_user_session
+    post   "login"       => "devise/sessions#create",  as: :user_session
+    delete "signout"     => "devise/sessions#destroy", as: :destroy_user_session
 
-    get    "signup"  => "registrations#new",     as: :new_user_registration
-    post   "signup"  => "registrations#create",  as: :user_registration
-    put    "signup"  => "registrations#update",  as: :update_user_registration
-    get    "account" => "registrations#edit",    as: :edit_user_registration
-    delete "account/:id" => "registrations#destroy", as: :delete_account
+    get    "signup"      => "registrations#new",       as: :new_user_registration
+    post   "signup"      => "registrations#create",    as: :user_registration
+    put    "signup"      => "registrations#update",    as: :update_user_registration
+    get    "account"     => "registrations#edit",      as: :edit_user_registration
+    delete "account/:id" => "registrations#destroy",   as: :delete_account
   end
 
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
@@ -40,16 +40,16 @@ Rails.application.routes.draw do
     end
   end
 
-  get 'cart', to: 'carts#show', as: 'cart'
-  delete 'cart/:id', to: 'carts#destroy', as: 'cart_delete'
+  get 'cart'           , to: 'carts#show'               , as: 'cart'
+  delete 'cart/:id'    , to: 'carts#destroy'            , as: 'cart_delete'
 
-  get 'order', to: 'orders#create_order', as: 'create_order'
-  get 'order/:id', to: 'orders#show', as: 'order'
-  post 'order/:id', to: 'orders#paid_order', as: 'paid_order'
-  delete 'order/:id', to: 'orders#cancel_order', as: 'cancel_order'
+  get 'order'          , to: 'orders#create_order'      , as: 'create_order'
+  get 'order/:id'      , to: 'orders#show'              , as: 'order'
+  post 'order/:id'     , to: 'orders#paid_order'        , as: 'paid_order'
+  delete 'order/:id'   , to: 'orders#cancel_order'      , as: 'cancel_order'
 
-  get 'get_address', to: 'locations#get_address'
-  get 'get_directions', to: 'locations#get_directions'
+  get 'get_address'    , to: 'locations#get_address'
+  get 'get_directions' , to: 'locations#get_directions'
 
   root 'products#index'
 
