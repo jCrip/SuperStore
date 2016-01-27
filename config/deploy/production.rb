15ba2dd902b79bbc0240425c92fe16782b12238f
Juan Cristobal Pazos <juancri@lalegion.cl>
2016-01-27 15:42:15 -0300

    fix db role

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :stage, :production
 set :rails_env, :production
 
-server '162.243.23.213', port: 22, user: 'deploy', roles: %w{web app}, primary: true
-server '162.243.23.213', port: 22, user: 'deployuser', roles: %w{db}
+role :db, user: 'deployuser'
+server '162.243.23.213', port: 22, user: 'deploy', roles: %w{web app db}, primary: true
