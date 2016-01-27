bc4c45673e1839e12b0a9d99b4c21a6d2ddd4061
Juan Cristobal Pazos <juancri@lalegion.cl>
2016-01-27 15:38:05 -0300

    change db role in deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
 set :stage, :production
 set :rails_env, :production
 
-server '162.243.23.213', port: 22, user: 'deploy', roles: %w{web app db}, primary: true
+server '162.243.23.213', port: 22, user: 'deploy', roles: %w{web app}, primary: true
+server '162.243.23.213', port: 22, user: 'deployuser', roles: %w{db}
