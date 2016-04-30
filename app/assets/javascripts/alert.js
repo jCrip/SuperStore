db99c391cccd01abb7817342fdf5f27961f587d5
Juan Cristobal Pazos <juancri@lalegion.cl>
2016-04-29 21:34:37 -0300

    otro commit

--- app/assets/javascripts/alert.js
+++ app/assets/javascripts/alert.js
@@ -1,7 +1,7 @@
-$(document).on('ready page:load page:update', function() {
-    setTimeout(function() {
-        $(".alert").fadeOut(function() {
-            $(this).remove();
-        })
-    }, 3000 );
-});
+// $(document).on('ready page:load page:update', function() {
+//     setTimeout(function() {
+//         $(".alert").fadeOut(function() {
+//             $(this).remove();
+//         })
+//     }, 3000 );
+// });
